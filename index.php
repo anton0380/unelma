8ff78e09865757b007d6687c3b87a64c4ea9129e
anton0380 <aa@unelma.ru>
2017-04-15 22:39:02 +0300

    add head in index

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+/**
+* The main template file
+*
+* @package WordPress
+* @subpackage Unelma
+* @since Unelma 1.0
+*/
+?><!DOCTYPE html>
+<!--[if IE 7]>
+<html class="ie ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 7) | !(IE 8) ]><!-->
+<html <?php language_attributes(); ?>>
+<!--<![endif]-->
+
+<head>
+   <meta charset="<?php bloginfo( 'charset' ); ?>">
+   <meta name="viewport" content="width=device-width">
+   <title><?php wp_title( '|', true, 'right' ); ?></title>
+   <link rel="profile" href="http://gmpg.org/xfn/11">
+   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+   <link rel='stylesheet' id='main-style' href='<?php echo get_stylesheet_uri(); ?>' type='text/css' media='all' />
+   <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="page" class="hfeed site">
+
+<!-- Header -->
+<header id="masthead" class="site-header" role="banner">
+<!-- Site name -->
+<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+<!-- Search form -->
+<div id="search-container" class="search-box-wrapper hide">
+<div class="search-box">
+<?php get_search_form(); ?>
+</div>
+</div>
+<!-- Menu -->
+<nav id="primary-navigation" class="site-navigation" role="navigation">
+<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+</nav>
+</header><!-- #masthead -->
+
+ 
+</body>
+</html>
