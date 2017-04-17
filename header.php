980164664dc49bfb989e2672493302d16575cc46
anton0380 <aa@unelma.ru>
2017-04-17 21:22:29 +0300

    add sidebar

--- header.php
+++ header.php
@@ -16,16 +16,18 @@
 <!--[if !(IE 7) | !(IE 8) ]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
-
-<head>
-   <meta charset="<?php bloginfo( 'charset' ); ?>">
-   <meta name="viewport" content="width=device-width">
-   <title><?php wp_title( '|', true, 'right' ); ?></title>
-   <link rel="profile" href="http://gmpg.org/xfn/11">
-   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-   <link rel='stylesheet' id='main-style' href='<?php echo get_stylesheet_uri(); ?>' type='text/css' media='all' />
-   <?php wp_head(); ?>
-</head>
+<div id="sidebar" class="sidebar">
+	<head>
+	   <meta charset="<?php bloginfo( 'charset' ); ?>">
+	   <meta name="viewport" content="width=device-width">
+	   <title><?php wp_title( '|', true, 'right' ); ?></title>
+	   <link rel="profile" href="http://gmpg.org/xfn/11">
+	   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	   <link rel='stylesheet' id='main-style' href='<?php echo get_stylesheet_uri(); ?>' type='text/css' media='all' />
+	   <?php wp_head(); ?>
+	</head>
+	<?php get_sidebar(); ?>
+</div><!-- .sidebar -->
 
 <body <?php body_class(); ?>>
 	<div id="page" class="hfeed site">
@@ -35,12 +37,12 @@
 			<!-- Site name -->
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
-			<!-- Search form -->
+			<!-- Search form 
 			<div id="search-container" class="search-box-wrapper hide">
 				<div class="search-box">
 					<?php get_search_form(); ?>
 				</div>
-			</div>
+			</div>-->
 			<!-- Menu -->
 			<nav id="primary-navigation" class="site-navigation" role="navigation">
 				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
